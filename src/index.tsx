d903900101bcc5ffd242e588268d39546dea1128
Ilya Smelkov <ismelkov@restream.rt.ru>
2017-06-07 16:15:57 +0300

    Simple level parse & display 👽

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,77 @@
-import * as ex from "excalibur";
+let game = new ex.Engine({
+  width: 800,
+  height: 600
+});
 
-let game = new ex.Engine({});
+class Wall extends ex.Actor {
+  constructor(x: number, y: number) {
+    super(x, y, 40, 40, ex.Color.Gray);
+    this.collisionType = ex.CollisionType.Fixed;
+  }
+}
+
+class Player extends ex.Actor {
+  constructor(x: number, y: number) {
+    super(x, y, 40, 40, ex.Color.White);
+    this.collisionType = ex.CollisionType.Fixed;
+  }
+}
+
+class Holder extends ex.Actor {
+  constructor(x: number, y: number) {
+    super(x, y, 40, 40, ex.Color.Black);
+    this.collisionType = ex.CollisionType.Passive;
+  }
+}
+
+class Box extends ex.Actor {
+  constructor(x: number, y: number) {
+    super(x, y, 40, 40, ex.Color.Orange);
+    this.collisionType = ex.CollisionType.Active;
+  }
+}
+
+class Level {
+  constructor(level: Array<string>) {
+    const offsetX = 50;
+    const offsetY = 50;
+
+    for (let i = 0; i < level.length; i++) {
+      for (let j = 0; j < level[i].length; j++) {
+        const x = offsetX + j * 40, y = offsetY + i * 40;
+
+        switch (level[i][j]) {
+          case "#":
+            game.add(new Wall(x, y));
+            break;
+          case "0":
+            game.add(new Box(x, y));
+            break;
+          case ".":
+            game.add(new Holder(x, y));
+            break;
+          case "@":
+            game.add(new Player(x, y));
+            break;
+        }
+      }
+    }
+  }
+}
+
+new Level([
+  "  ######",
+  "  # ..@#",
+  "  # 00 #",
+  "  ## ###",
+  "   # #  ",
+  "   # #  ",
+  "#### #  ",
+  "#    ## ",
+  "# #   # ",
+  "#   # # ",
+  "###   # ",
+  "  ##### "
+]);
+
+game.start();
